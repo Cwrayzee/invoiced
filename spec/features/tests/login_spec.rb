f1c016ec27eeda2a92efd493f5ec6dc58e68b041
Cwrayzee <wray.crawford@gmail.com>
2020-06-30 00:25:48 -0500

    set number of times to run for user lockout

--- spec/features/tests/login_spec.rb
+++ spec/features/tests/login_spec.rb
@@ -2,6 +2,12 @@
 describe 'Login to Invoiced demo app', type: :feature, js: true do
   include PagesHelper
 
+  before :all do
+    creds = YAML.load_file('spec/test_data/login_data.yml')
+    @username = creds['username']
+    @password = creds['password']
+  end
+
   before :each do
     visit '/login'
   end
@@ -16,7 +22,7 @@ describe 'Login to Invoiced demo app', type: :feature, js: true do
 
   context "Valid login data" do
     it 'allows user to login with valid username and password' do
-      login_page.login_user('wray.crawford@gmail.com', 'Admin*123')
+      login_page.login_user(@username, @password)
       while page.has_css?('.pg-loading')
         sleep 0.1
       end
@@ -26,21 +32,22 @@ describe 'Login to Invoiced demo app', type: :feature, js: true do
 
   context 'Invalid Login data' do
     it 'shows invalid message and console error with bad email' do
-      login_page.login_user('bad_email@example.com', 'Admin*123')
+      login_page.login_user('bad_email@example.com', @password)
       expect(login_page.invalid_login_alert_message).to eq("We could not find a match for that email address and password.")
       expect{ console_check }.to raise_error(JavaScriptConsoleError)
     end
 
     it 'shows invalid message and console error with bad password' do
-      login_page.login_user('wray.crawford@gmail.com', 'badPassword')
+      login_page.login_user(@username, 'badPassword')
       expect(login_page.invalid_login_alert_message).to eq("We could not find a match for that email address and password.")
       expect{ console_check }.to raise_error(JavaScriptConsoleError)
     end
 
     it 'temporarily locks the user out after too many invalid attempts' do
-      login_page.login_user('wray.crawford@gmail.com', 'badPassword')
-      refresh_browser
-      login_page.login_user('wray.crawford@gmail.com', 'badPassword')
+      3.times do
+        login_page.login_user(@username, 'badPassword')
+        refresh_browser
+      end
       lockout_message = "This account has been locked due to too many failed sign in attempts."
       expect(login_page.invalid_login_alert_message).to include(lockout_message)
     end
